7b9e92c4d1f8166c1f993d16873b02571f9f786e
cmartin <cmartin@106-15.info2>
2022-09-21 13:24:58 +0200

    commit1

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		System.out.print("This is fine");
+		System.out.println("He said while the house was burning");
+
+	}
+
+}
