b0602f7b15a86cff05f07fdcc6e0f599e0c2a7ad
cmartin <cmartin@106-15.info2>
2022-09-21 13:27:23 +0200

    commit2

---
+++ Exercise12/src/exercise2/Exercise2.java
@@ -0,0 +1,12 @@
+package exercise2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		System.out.println("Easy, peasy lemon esqueezy");
+		System.out.println("Thant's nice");
+
+
+	}
+
+}
---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,13 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Carmen Martín Núñez");
+		System.out.println("I live in the avenue of 'La Palmera', 17 in Seville");
+		System.out.println();
+		System.out.println("My phone number is 653532779");
+
+	}
+
+}
